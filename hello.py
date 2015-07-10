5e087c041c5e6c81631cea6a8434880c2b6228c5
yiyiarrow <yiyiarrow@163.com>
2015-07-10 22:49:44 +0800

    Revert "add end"
    This reverts commit 7a8593e2ed90ca4a07567daeea67cf6962a57c31.

--- hello.py
+++ hello.py
@@ -4,9 +4,7 @@
 import os
 import time
 
-print "----------begin-------------"
 print "Hello,", os.name, "! --", time.strftime("%Z %Y-%m-%d %A %H:%M:%S", time.localtime(time.time()))
-print "---------- end -------------"
 
 raw_input("click any key to quit...")
 
